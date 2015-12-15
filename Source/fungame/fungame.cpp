d03774e42b1e22e2c071808c244e910376faafb0
Jean-Simon Collard <jeansimoncollard@hotmail.com>
2015-12-15 17:58:35 -0500

    source added

---
+++ Source/fungame/fungame.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "fungame.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, fungame, "fungame" );
